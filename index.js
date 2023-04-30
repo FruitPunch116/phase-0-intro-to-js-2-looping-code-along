03eb843581cfe03544e26909519672958f624f46
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-04-30 02:15:09 -0400

    Looping Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+let surprise = "surprise";
+
+function writeCards(names, surprise) {
+    const messages = [];
+   for (let i = 0; i < names.length; i ++) { 
+    const message = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`;
+    messages.push(message);
+    console.log(message);
+    }
+console.log(messages);
+    return messages;
+}
+
+function countDown() {
+    let counter = 10;
+    while (counter >= 0) {
+    console.log(counter);
+    counter --;
+}
+return countDown;
+}
+
+countDown(4);
+console.log(countDown(4));
